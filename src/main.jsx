13eff023225af92c71945bc32cb505d96324ec36
Mica <micabeacabrera@gmail.com>
2024-05-30 00:52:31 -0300

    start

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CssBaseline from '@mui/material/CssBaseline';
+import App from './App.jsx'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import './index.css'
+import { ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme({  
+
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#180733',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+  })
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <ThemeProvider theme={theme}> 
+  <CssBaseline />
+    <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+)
